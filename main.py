68b70a5242cba3a09431aa13027191c6584cc941
igarashi339 <igarashi339@gmail.com>
2024-08-17 11:49:16 +0900

    不要なコードを削除

--- main.py
+++ main.py
@@ -5,28 +5,16 @@ from libs.spreadsheet import SpreadSheetAccessor
 
 
 ACCESS_SPAN_SEC = 10  # 秒
-BASE_URL = "https://www.sej.co.jp"
 
 
 def fetch_product_list():
     product_list = []
     for week_str in ["thisweek", "nextweek"]:
-        url = f"{BASE_URL}/products/a/{week_str}/area/kanto/1/l100/"
+        url = f"https://www.sej.co.jp/products/a/{week_str}/area/kanto/1/l100/"
         product_list_tmp = exec_single_week(url)
         product_list.extend(product_list_tmp)
         time.sleep(ACCESS_SPAN_SEC)
     return product_list
-
-
-def fetch_product_detail_list(product_list: list):
-    product_detail_list = []
-    for product in product_list:
-        product_detail = exec_single_product(BASE_URL + product["url"])
-        product.update(product_detail)
-        product_detail_list.append(product.copy())
-        pprint.pprint(product_detail)
-        time.sleep(ACCESS_SPAN_SEC)
-    return product_detail_list
 
 
 def main():
