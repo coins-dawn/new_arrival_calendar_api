07bbfa248320afbe076b5b8e29f63a8552d91833
igarashi339 <igarashi339@gmail.com>
2024-09-15 13:34:30 +0900

    画像が存在しない場合のケア

--- main.py
+++ main.py
@@ -23,15 +23,17 @@ def main():
     image_uploader = ImageUploader()
     sheet_matrix = ssaccessor.fetch_all_records()
     product_id_set = {row[0] for row in sheet_matrix}
-    
+
     product_list = fetch_product_list()
-    
+
     for product in product_list:
         if product.id in product_id_set:
             continue
         product_detail = exec_single_product(product)
-        file_id = image_uploader.download_and_upload_image(product_detail.image_url)
-        image_url = "https://lh3.googleusercontent.com/d/" + file_id
+        image_url = ""
+        if product_detail.image_url != "":
+            file_id = image_uploader.download_and_upload_image(product_detail.image_url)
+            image_url = "https://lh3.googleusercontent.com/d/" + file_id
         ssaccessor.append_product_detail(product_detail, image_url)
         time.sleep(ACCESS_SPAN_SEC)
 
